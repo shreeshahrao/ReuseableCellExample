f7c3f6e49c62ac2352265db21a69892edc0e0859
Shreesha <shreeshahrao1999@gmail.com>
2022-01-02 21:27:45 +0530

    Initial Commit

---
+++ ReuseableCellExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReuseableCellExample
+//
+//  Created by Shreesha Rao on 02/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ReuseableCellExampleUITests/ReuseableCellExampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ReuseableCellExampleUITestsLaunchTests.swift
+//  ReuseableCellExampleUITests
+//
+//  Created by Shreesha Rao on 02/01/22.
+//
+
+import XCTest
+
+class ReuseableCellExampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
